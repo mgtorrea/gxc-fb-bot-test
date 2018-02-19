9c31b93f0c0c2983eaee03a24363a78cc8d60eda
Mario Alberto García <mgtorrea@users.noreply.github.com>
2018-02-19 11:14:44 -0600

    Update WebhookHandler.java

--- src/main/java/org/mx/geexco/test/bots/fb/WebhookHandler.java
+++ src/main/java/org/mx/geexco/test/bots/fb/WebhookHandler.java
@@ -1,21 +1,36 @@
 package org.mx.geexco.test.bots.fb;
 
+import java.net.URL;
+import java.util.Arrays;
 import java.util.Deque;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import com.github.messenger4j.Messenger;
+import com.github.messenger4j.common.WebviewHeightRatio;
+import com.github.messenger4j.common.WebviewShareButtonState;
 import com.github.messenger4j.send.MessagePayload;
+import com.github.messenger4j.send.MessagingType;
+import com.github.messenger4j.send.message.TemplateMessage;
 import com.github.messenger4j.send.message.TextMessage;
+import com.github.messenger4j.send.message.template.GenericTemplate;
+import com.github.messenger4j.send.message.template.button.Button;
+import com.github.messenger4j.send.message.template.button.CallButton;
+import com.github.messenger4j.send.message.template.button.PostbackButton;
+import com.github.messenger4j.send.message.template.button.UrlButton;
+import com.github.messenger4j.send.message.template.common.DefaultAction;
+import com.github.messenger4j.send.message.template.common.Element;
+import com.github.messenger4j.webhook.Event;
 
 import io.undertow.util.HeaderMap;
 
 public class WebhookHandler extends AbstractJSONProcessorHandler {
 
 	private String VERIFY_TOKEN = System.getProperty("VERIFY_TOKEN");
-	private String PAGE_ACCESS_TOKEN=System.getProperty("PAGE_ACCESS_TOKEN");
-	private String APP_SECRET=System.getProperty("APP_SECRET");	
-	
+	private String PAGE_ACCESS_TOKEN = System.getProperty("PAGE_ACCESS_TOKEN");
+	private String APP_SECRET = System.getProperty("APP_SECRET");
+
 	final Messenger messenger;
 
 	public WebhookHandler() {
@@ -49,22 +64,56 @@ public class WebhookHandler extends AbstractJSONProcessorHandler {
 		return "EVENT_RECEIVED";
 	}
 
+	private void sendWelcomeMessage(String senderId, Event event) throws Exception {
+		final List<Button> buttons = Arrays.asList(
+				UrlButton.create("Aviso de privacidad", new URL("http://www.google.com")),
+				CallButton.create("Línea Telefónica", "+15105551234"),
+				PostbackButton.create("Duda sobre X concurso", "DUDAS_CONCURSO"),
+				PostbackButton.create("Duda sobre Y taller", "DUDAS_TALLER"),
+				PostbackButton.create("Transparencia", "DUDAS_TRANSP"));
+
+		final DefaultAction defaultAction = DefaultAction.create(
+				new URL("https://peterssendreceiveapp.ngrok.io/view?item=103"), Optional.of(WebviewHeightRatio.TALL),
+				Optional.of(true), Optional.of(new URL("https://peterssendreceiveapp.ngrok.io/")),
+				Optional.of(WebviewShareButtonState.HIDE));
+
+		final Element element = Element.create("Bienvenido a XXYYZZ",
+				Optional.of("Subtítulo o saludo adicional. ¿En qué podemos ayudarte?"), Optional.empty(),
+				Optional.of(defaultAction), Optional.of(buttons));
+
+		final GenericTemplate genericTemplate = GenericTemplate.create(Arrays.asList(element));
+
+		final MessagePayload payload = MessagePayload.create(senderId, MessagingType.RESPONSE,
+				TemplateMessage.create(genericTemplate));
+
+		messenger.send(payload);
+	}
+
+	private void sendSimpleTextMessage(String senderId, Event event) throws Exception {
+		final String text = event.asTextMessageEvent().text();
+		LOG.info("MENSAJE DE TEXTO RECIBIDO DEL USUARIO CON ID [" + senderId + "]: " + text);
+		final TextMessage textMessage = TextMessage.create("==>" + text.toUpperCase() + "<==");
+		final MessagePayload messagePayload = MessagePayload.create(senderId, MessagingType.RESPONSE, textMessage);
+		try {
+			messenger.send(messagePayload);
+		} catch (Exception e) {
+			LOG.error("Error al enviar el mensaje", e);
+		}
+	}
+
 	private void processPayload(String payload, String signature) {
 
 		try {
 			messenger.onReceiveEvents(payload, Optional.of(signature), event -> {
 				final String senderId = event.senderId();
+				try {
+					sendWelcomeMessage(senderId, event);
+				} catch (Exception e) {
+					LOG.error("Error al enviar el mensaje de bienvenida.");
+				}
 
 				if (event.isTextMessageEvent()) {
-					final String text = event.asTextMessageEvent().text();
-					LOG.info("MENSAJE DE TEXTO RECIBIDO DEL USUARIO CON ID ["+senderId+"]: "+text);
-					final TextMessage textMessage = TextMessage.create("==>" + text.toUpperCase() + "<==");
-					final MessagePayload messagePayload = MessagePayload.create(senderId, textMessage);
-					try {
-						messenger.send(messagePayload);
-					} catch (Exception e) {
-						LOG.error("Error al enviar el mensaje", e);
-					}
+
 				} else {
 					LOG.warn("Evento no manejado: " + event.getClass());
 				}

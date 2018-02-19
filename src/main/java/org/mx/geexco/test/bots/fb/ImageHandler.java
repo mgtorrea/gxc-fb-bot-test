b1f043df46ecc1c09772d33046b6e1497d907576
Mario Alberto García <mgtorrea@users.noreply.github.com>
2018-02-19 13:35:57 -0600

    Create ImageHandler.java

---
+++ src/main/java/org/mx/geexco/test/bots/fb/ImageHandler.java
@@ -0,0 +1,30 @@
+package org.mx.geexco.test.bots.fb;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+public class ImageHandler implements HttpHandler {
+
+	@Override
+	public void handleRequest(HttpServerExchange hse) throws Exception {
+		if (hse.isInIoThread()) {
+			hse.dispatch(this);
+			return;
+		}
+		// Obtiene entrada POST
+		hse.startBlocking();
+		InputStream is= ImageHandler.class.getClassLoader().getResourceAsStream("header.png");
+		hse.getResponseHeaders().put(Headers.CONTENT_TYPE, "image/png");
+
+		IOUtils.copy(is, hse.getOutputStream());
+		hse.endExchange();
+		hse.unDispatch();
+
+	}
+
+}
